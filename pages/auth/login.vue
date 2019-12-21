b81663db9f2a7a1da9f227a322f57f61f1186e87
Elias Renman <elias.renman00@gmail.com>
2019-12-21 19:59:07 +0100

    tried changing stuff

---
+++ pages/auth/login.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container">
+        <div class="col-md-6 offset-md-3 mt-4">
+
+            <div v-if="error" class="alert alert-danger mb-2" role="alert">
+                Your token appeared to be invalid. Please try again.
+            </div>
+
+            <social-login />
+
+            <div class="card">
+                <div class="card-header">
+                    <p class="mb-0">Login</p>
+                </div>
+                <div class="card-body">
+                    <form @submit.prevent="login">
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input v-model="form.email" type="email" class="form-control" :class="{ 'is-invalid': errors.email }" placeholder="Email">
+                            <div class="invalid-feedback" v-if="errors.email">
+                                {{errors.email[0]}}
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input v-model="form.password" type="password" class="form-control" :class="{ 'is-invalid': errors.password }" placeholder="Password">
+                            <div class="invalid-feedback" v-if="errors.password">
+                                {{errors.password[0]}}
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" value="Login" class="btn btn-light w-100">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import SocialLogin from '@/components/SocialLogin';
+export default {
+    middleware: 'guest',
+    components: {
+        SocialLogin
+    },
+    data() {
+        return {
+            form: {
+                email: '',
+                password: ''
+            },
+            error: this.$route.query.error
+        }
+    },
+    methods: {
+        async login() {
+            try {
+                await this.$auth.login({ data: this.form });
+            } catch(e) {
+                return;
+            }
+            this.$router.push(this.$route.query.redirect ? this.$route.query.redirect : '/');
+        }
+    }
+}
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,14 +1,19 @@
 <template>
-  <login-form></login-form>
+    <div>
+        <social-login />
+        <login-form></login-form>
+    </div>
 </template>
 
 <script>
 
 import LoginForm from '~/components/LoginForm.vue';
+import SocialLogin from '~/components/SocialLogin';
 
 export default {
     components: {
-        LoginForm
+        LoginForm,
+        SocialLogin
     }
 }
 </script>
