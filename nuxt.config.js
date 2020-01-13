f40ba95d4ab1e6f5f03ad9d603374fc139c9a652
ludvig.bjorkman <luddenr1@live.se>
2020-01-13 10:34:27 +0100

    Added scss variables. Temporary Success page and no_event  page, will be refactored.

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,9 +13,10 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { 
+      {
         rel: 'icon', type: 'image/x-icon', href: '/favicon.ico',
-        rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Titillium+Web&display=swap"' }
+        rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Titillium+Web&display=swap',
+        rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:400,800&display=swap' },
     ]
   },
   /*
@@ -57,19 +58,19 @@ export default {
     strategies: {
       local: {
         endpoints: {
-          login: { 
-            url: '/auth/login', 
-            method: 'post', 
-            propertyName: 'access_token' 
+          login: {
+            url: '/auth/login',
+            method: 'post',
+            propertyName: 'access_token'
           },
-          logout: { 
-            url: '/auth/logout', 
-            method: 'post' 
+          logout: {
+            url: '/auth/logout',
+            method: 'post'
           },
-          user: { 
-            url: '/auth/user', 
-            method: 'get', 
-            propertyName: 'user' 
+          user: {
+            url: '/auth/user',
+            method: 'get',
+            propertyName: 'user'
           }
         },
         // tokenRequired: true,
---
+++ pages/success.vue
@@ -0,0 +1,28 @@
+<template>
+    <center-wrapper>
+        <div>
+        <img src="~/assets/icons/checkmark.svg" class="image" alt="Dropdown">
+        <h1> Här var det tomt! </h1>
+        <p> För nuvarande finns det inget event. Kolla tillbaka senare! </p>
+        </div>
+    </center-wrapper>
+
+</template>
+<script>
+    import CenterWrapper from '~/components/CenterWrapper.vue';
+
+    export default {
+    components: {
+        CenterWrapper
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+.image {
+  height: 400px;
+  width: 400px;
+}
+</style>
+
