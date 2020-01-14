c781ea195d6948bcc90e723eb2b80259d6d2ca0d
Elias Renman <elias.renman00@gmail.com>
2020-01-14 19:00:31 +0100

    finished up wireframing of basic functionallity

--- pages/event/index.vue
+++ pages/event/index.vue
@@ -1,11 +1,13 @@
 <template>
-  
+    <div>
+        <h1>Index page for event</h1>
+    </div>
 </template>
 
 <script>
 //TODO: Make this the no event exists at the moment page.
 export default {
-
+    middleware: 'auth',
 }
 </script>
 
