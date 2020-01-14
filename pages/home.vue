dbb212192c806cd1100e9bb7a615d2d36e17427b
ludvig.bjorkman <luddenr1@live.se>
2020-01-14 11:24:45 +0100

    Google button :D

--- pages/home.vue
+++ pages/home.vue
@@ -1,14 +1,23 @@
 <template>
     <center-wrapper >
-        <div class="text-center">
+        <div>
             <image-layout
                 icon="nti_small_black.svg"
                 alt="Bild på NTIs logga, Välkommen!"
                 title="Välkommen!"
                 paragraph="Logga in nedan för att komma vidare i appen."
             />
+              <div class="justify-content-center d-flex">
 
-            <button type="button" class="google" href="#">Log in with google</button>
+                <div class="google-button" >
+                  <span class="google-inner">
+
+                    <img src="~/assets/images/google_button.svg" class="image"> </img>
+                    <p>Logga in med google</p>
+                  </span>
+                </div>
+              </div>
+
         </div>
     </center-wrapper>
 
@@ -26,15 +35,34 @@
 
 <style lang="scss" scoped>
 
-.google {
-    width: 343px;
-    height: 59px;
+.google-button {
+    max-width: 343px;
 
     background: #FFFFFF;
     /* Shadow-button */
     box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.1);
     border-radius: 10px;
 }
+.google-button:hover {
+    box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.3);
+}
+.google-inner {
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    padding: 5px 10px;
+    p {
+      margin: 0;
+      padding-top: 10px;
+      font-size: 24px;
+      text-transform: uppercase;
+    }
+}
+
+.image {
+  margin: 10px;
+}
+
 
 </style>
 
