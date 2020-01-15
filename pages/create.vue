678f13f47486f4f370623cdd515132aaa7ad9f89
ludvig.bjorkman <luddenr1@live.se>
2020-01-15 13:12:53 +0100

    Create form page completed

--- pages/create.vue
+++ pages/create.vue
@@ -1,63 +1,77 @@
 //TODO: Fix borked placements of forms.
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <h1> Information </h1>
-                <form>
-                    <div class="form-group">
-                      <label for="lan-start">Lanet börjar:</label>
-                        <v-date-picker id="lan-start"
-                            :min-date='new Date()'
-                            v-model="eventDates.dates"
-                            mode="range"
-                            placeholder="Välj datum"
+<div class="container pt-4">
+    <div class="row">
+        <div class="col-md-4">
+            <h1>Tider</h1>
+            <label for="lan-start">Lanet börjar:</label>
+            <div class="d-flex">
 
-                        />
-                        <time-picker
-                            hide-disabled-items
-                            :minute-interval="5"
-                            hour-label="Timme" minute-label="Minut"
-                            v-model="eventDates.startTime"
-                        />
-                        <label for="last-registration-date">Sista anmälningsdatum.</label>
-                        <v-date-picker id="last-registration-date"
-                            :min-date='new Date()'
-                            v-model="registerClosure.date"
-                            placeholder="Välj datum"
+                <v-date-picker id="lan-start"
+                        :min-date='new Date()'
+                        v-model="eventDates.dates"
+                        mode="range"
+                        placeholder="Välj datum"
 
-                        />
-                        <time-picker
-                            hide-disabled-items
-                            :minute-interval="5"
-                            hour-label="Timme" minute-label="Minut"
-                            v-model="registerClosure.time"
-                        />
-                    </div>
-                </form>
+                />
+                <time-picker
+                    hide-disabled-items
+                    :minute-interval="5"
+                    hour-label="Timme" minute-label="Minut"
+                    v-model="eventDates.startTime"
+                />
             </div>
-            <div class="col-md-4">
-                <h1>Ytterligare information </h1>
-                    <div class="form-group">
-                    <textarea class="form-control" rows="3"></textarea>
-                    </div>
-                </div>
-            <div class="col-md-4">
-                <h1>Regler </h1>
-                <form>
-                    <div class="form-group">
-                    </div>
-                </form>
+
+            <label for="last-registration-date">Sista anmälningsdatum.</label>
+            <div class="d-flex">
+
+                <v-date-picker id="last-registration-date"
+                    :min-date='new Date()'
+                    v-model="registerClosure.date"
+                    placeholder="Välj datum"
+
+                />
+                <time-picker
+                    hide-disabled-items
+                    :minute-interval="5"
+                    hour-label="Timme" minute-label="Minut"
+                    v-model="registerClosure.time"
+                />
             </div>
         </div>
+        <div class="col-md-4 order-md-last">
+            <h1>Regler <br> </h1>
+            <div style="height:100%">
+                <rules-input> </rules-input>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <h1>Information </h1>
+            <textarea class="form-control" rows="10"></textarea>
+
+            <div class="m-0 py-4 d-flex justify-content-center">
+
+                <action-button
+                    title="Publicera Event"
+                    icon="done"
+                    primary="true"
+                />
+            </div>
+        </div>
+
     </div>
+</div>
 </template>
 
 <script>
 import timePicker from 'vue2-timepicker'
+import RulesInput from '~/components/event/RulesInput'
+import ActionButton from '~/components/buttons/ActionButton'
     export default {
     components: {
       timePicker,
+      RulesInput,
+      ActionButton,
     },
     data(){
       return {
@@ -89,15 +103,10 @@ import timePicker from 'vue2-timepicker'
 </style>
 
 <style lang="scss" scoped>
-
-.container {
-    margin-top: 50px;
+h1 {
+  padding: 10px 0;
 }
-.col-md-3 {
-    margin-right: 100px;
-
-}
-.form-group {
-  display: inline-block;
+textarea {
+  resize: none;
 }
 </style>
