21284032ac87c8f73207e5cd8b3b2f54a21c0ab7
Elias Renman <33152039+eliasrenman@users.noreply.github.com>
2020-01-15 08:43:25 +0100

    Added gradient animation to action button and input field (#12)
    * added gradient animation to action button and input field
    
    * Update ActionButton.vue

--- components/login/InputField.vue
+++ components/login/InputField.vue
@@ -15,7 +15,7 @@
             :name="name"
             :id="id"
         >
-        <div :class='{"active": active}' class="line gradient--background" />
+        <div :class='{"gradient-animation-active": active}' class="line gradient-animation" />
       </div>
   </div>
 </template>
@@ -55,9 +55,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.active {
-    background: #E1259C !important;
-}
 
 .y-margin {
     padding: 10px 0;
