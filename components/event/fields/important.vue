d060d23ca858cbd62f9a2ba490c07d6807601bee
Elias Renman <elias.renman00@gmail.com>
2020-01-15 10:21:37 +0100

    added stuff

---
+++ components/event/fields/important.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="d-flex">
+      <p v-text="title" class="my-1 mr-2"/>
+      <p class="font-weight-bold my-1 ">
+          <slot/>
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'title'
+    ]
+}
+</script>
+
+<style>
+
+</style>
--- pages/event/register.vue
+++ pages/event/register.vue
@@ -1,13 +1,19 @@
 <template>
 <center-wrapper>
-    <wrapper title="NU ÄR DET DAGS FÖR LAN!"> 
-    <p>Nu är det dags för lan!
-        För externer (icke elever) kostar det 50kr
-        Lanet öppnar 16:30
-        När: 2019-12-12 - 2020-01-01
-        Anmälan stänger 2019-12-10 14:05
-    </p>
-
+    <wrapper title="NU ÄR DET DAGS FÖR LAN!">
+        <div>
+            <p>Nu är det dags för lan! För externer (icke elever) kostar det 50kr</p>
+            <important title="Lanet öppnar: ">16:30</important>
+            <important title="När:">2019-12-12 - 2020-01-01</important>
+            <important title="Anmälan stänger:">2019-12-10 14:05</important>
+            <div>
+                <h3>Innan du anmäler dig</h3>
+                <p class="d-inline">Se till att du läser </p>
+                <p class="d-inline clickable underline">lanets regler</p>
+                <p class="d-inline">innan du anmäler dig, reglerna går även att läsa efter du anmält dig.</p>
+            </div>
+            <action-button title="TILL ANMÄLAN"></action-button>
+        </div>
     </wrapper>
 </center-wrapper>
 </template>
@@ -16,6 +22,7 @@
 import Wrapper from '~/components/layouts/positioning/Wrapper.vue';
 import CenterWrapper from '~/components/layouts/positioning/CenterWrapper.vue';
 import InputField from '~/components/login/InputField.vue';
+import Important from '~/components/event/fields/important';
 import ActionButton from '~/components/buttons/ActionButton.vue';
 
 export default {
@@ -23,7 +30,8 @@ export default {
         Wrapper,
         CenterWrapper,
         InputField,
-        ActionButton
+        ActionButton,
+        Important
     },
 }
 </script>
