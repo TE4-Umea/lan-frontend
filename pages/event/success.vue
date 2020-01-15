875530212e50e564c2abbd326e2d1c11c258538c
ludvig.bjorkman <luddenr1@live.se>
2020-01-15 14:14:31 +0100

    Added create to event folder, and worked a b it on success.vue

--- pages/event/success.vue
+++ pages/event/success.vue
@@ -1,6 +1,6 @@
 <template>
     <center-wrapper >
-        <div>
+
         <image-layout
             icon="checkmark.svg"
             alt="Lyckades!"
@@ -8,21 +8,29 @@
             paragraph=""
         >
         <p class="d-inline ">
-            Du är anmäld till lanet som elev mellan <p class="d-inline font-weight-bold">2019-12-12 - 2020-01-01</p>, lanet börjar <p class="d-inline font-weight-bold">kl 16:30</p>
-        </p>
+            Du är anmäld till lanet som elev mellan </p> <p class="d-inline font-weight-bold">2019-12-12 - 2020-01-01</p>, lanet börjar <p class="d-inline font-weight-bold">kl 16:30</p>
         </image-layout>
-        
+        <div class="pt-4 d-flex justify-content-center">
+            <action-button
+                title="Din biljett"
+                icon="local_play"
+                primary="true"
+            />
+
         </div>
+
     </center-wrapper>
 
 </template>
 <script>
     import CenterWrapper from '~/components/layouts/positioning/CenterWrapper';
     import ImageLayout from '~/components/layouts/ImageLayout'
+    import ActionButton from '~/components/buttons/ActionButton'
     export default {
     components: {
             CenterWrapper,
-            ImageLayout
+            ImageLayout,
+            ActionButton,
         }
     }
 </script>
