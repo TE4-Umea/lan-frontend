f2a316a5d2625db1a52bcb8436e9611a02c0058e
Elias Renman <33152039+eliasrenman@users.noreply.github.com>
2020-01-15 08:43:37 +0100

    Feature new Success svg (#11)
    * fixed input field bug
    
    * New success svg!

--- components/login/InputField.vue
+++ components/login/InputField.vue
@@ -14,6 +14,7 @@
             v-model="data"
             :name="name"
             :id="id"
+            :key="id"
         >
         <div :class='{"gradient-animation-active": active}' class="line gradient-animation" />
       </div>
