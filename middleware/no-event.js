dd402667c856f31920d8a1c8a5bde77d070bfc7a
Elias Renman <elias.renman00@gmail.com>
2020-01-16 14:12:17 +0100

    moved create

--- middleware/no-event.js
+++ middleware/no-event.js
@@ -1,4 +1,8 @@
 export default function({ $auth, redirect }) {
+    let user = $auth.$state.user;
+    if((user && user.admin)) {
+        return redirect('/admin/event/create');
+    }
     if ($auth.$state.event == undefined) {
         return redirect('/event/none');
     }
