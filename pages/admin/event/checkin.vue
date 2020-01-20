98c8d378b20a1c1d66d650d2b134db5b6db1fa6a
ludvig.bjorkman <luddenr1@live.se>
2020-01-20 13:50:51 +0100

    Added admin checkin page.  With functioning QR code reader.

--- pages/admin/event/checkin.vue
+++ pages/admin/event/checkin.vue
@@ -1,16 +1,52 @@
 <template>
-  
+    <center-wrapper>
+        <wrapper>
+            <h1> Check in </h1>
+            <input type="text" class="form-control"   placeholder="Lägg till ID manuellt...." v-model="result">'
+            <qrcode-stream @decode="onDecode"></qrcode-stream>
+
+
+
+
+
+        </wrapper>
+    </center-wrapper>
 </template>
 
 <script>
+import Wrapper from '~/components/layouts/positioning/Wrapper';
+import CenterWrapper from '~/components/layouts/positioning/CenterWrapper';
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
 export default {
     middleware: [
         'auth-admin',
         'event/none'
-    ]
+    ],
+
+    data () {
+      return {
+        result: '',
+        noStreamApiSupport: false
+      }
+  },
+
+  components: {
+      Wrapper,
+      CenterWrapper,
+      QrcodeStream,
+      QrcodeDropZone,
+      QrcodeCapture,
+  },
+  methods: {
+    onDecode (result) {
+        this.result = result
+        },
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+
 
 </style>
