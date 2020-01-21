52be79dda75d7f855acbee9c6064ef5c44c7a4f5
Ludvig Björkman <31842232+luddoro@users.noreply.github.com>
2020-01-21 08:37:14 +0100

    Feature ludvig björkman admin check in (#21)
    * Added create to event folder, and worked a b it on success.vue
    
    * Started on event registration success page
    
    * Anmälningsbekräftelse klar. Buggad stängningsknapp på modal.
    
    * "De ser bra ut de här" - Elias  2020
    
    * added stuff
    
    * added more middleware
    
    * simple fade in animation
    
    * fine commit
    
    Co-authored-by: Elias Renman <elias.renman00@gmail.com>
    
    * moving animations
    
    * fixed shit
    
    * Moved animation to transitions. Event registration now has fully functioning animations
    
    * Added admin checkin page.  With functioning QR code reader.
    
    * Added button for activating / deactivating the camera.
    
    * Just basic CSS shenanigans
    
    Co-authored-by: Elias Renman <33152039+eliasrenman@users.noreply.github.com>

--- pages/admin/event/checkin.vue
+++ pages/admin/event/checkin.vue
@@ -1,16 +1,78 @@
 <template>
-  
+    <center-wrapper>
+        <wrapper>
+          <h1> Check in </h1>
+            <div class ="d-flex align-items-center">
+                <input type="text" class="form-control"   placeholder="Lägg till ID manuellt...." v-model="result">
+                <div class="box gradient-animation-hover box-padding">
+                    <i class="icon material-icons clickable primary-color"
+                        v-text="icon"
+                        v-on:click= "handleClick"
+                    />
+                </div>
+            </div>
+            <qrcode-stream v-if="showCamera" @decode="onDecode"></qrcode-stream>
+        </wrapper>
+    </center-wrapper>
 </template>
 
 <script>
+import Wrapper from '~/components/layouts/positioning/Wrapper';
+import CenterWrapper from '~/components/layouts/positioning/CenterWrapper';
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
 export default {
     middleware: [
         'auth-admin',
         'event/none'
-    ]
-}
+    ],
+
+    data () {
+        return {
+            showCamera: false,
+            result: '',
+            noStreamApiSupport: false,
+            icon: 'camera_enhance'
+        }
+    },
+
+
+    components: {
+        Wrapper,
+        CenterWrapper,
+        QrcodeStream,
+        QrcodeDropZone,
+        QrcodeCapture,
+    },
+    methods: {
+        onDecode (result) {
+            this.result = result
+        },
+        handleClick: function(event) {
+            if(!this.showCamera)  {
+                this.showCamera = true;
+            }else {
+              this.showCamera = false;
+            }
+        }
+    },
+  }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+.box {
+    padding: 5px;
+    margin-left: 10px;
+    border-radius: 6p;
+}
+
+.wrapper {
+    margin-top: 25px;
+}
+
+.icon {
+  color: white;
+}
+
 
 </style>
