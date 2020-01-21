3665bd79d00e659c90ff02de4e16c5692683ae5a
Elias Renman <elias.renman00@gmail.com>
2020-01-21 08:40:44 +0100

    fixed ticket and ticket success page

--- pages/event/ticket.vue
+++ pages/event/ticket.vue
@@ -1,35 +1,83 @@
 <template>
     <center-wrapper >
         <div>
-        <image-layout
-            icon="checkmark.svg"
-            alt="Lyckades!"
-            title="Lyckades!"
-            paragraph=""
-        >
-        <p class="d-inline ">
-            Du är anmäld till lanet som elev mellan <p class="d-inline font-weight-bold">2019-12-12 - 2020-01-01</p><p class="d-inline">, lanet börjar </p><p class="d-inline font-weight-bold">kl 16:30</p>
-        </p>
-        </image-layout>
-        
+            <image-layout
+                icon="checkmark.svg"
+                alt="Lyckades!"
+                title="Lyckades!"
+                paragraph=""
+            >
+            <p class="d-inline ">
+            Du är anmäld till lanet som</p> 
+            <p class="d-inline font-weight-bold" v-text="account_type_str" />
+            <p class="d-inline"> mellan </p>
+            <p class="d-inline font-weight-bold">{{betweenDates}}</p>
+            <p class="d-inline">, lanet börjar </p>
+            <p class="d-inline font-weight-bold">{{ 'kl ' + opensAt}}</p>
+            
+            
+            </image-layout>
+            <div class="pt-4 d-flex justify-content-center">
+                <action-button
+                    title="Din biljett"
+                    icon="local_play"
+                    primary="true"
+                    @onAction="openTicket"
+                />
+
+            </div>
         </div>
     </center-wrapper>
 
 </template>
 <script>
-    import CenterWrapper from '~/components/layouts/positioning/CenterWrapper';
-    import ImageLayout from '~/components/layouts/ImageLayout'
-    export default {
-        middleware: [
-            'registration/none',
-            'registration/checked-in',
+import CenterWrapper from '~/components/layouts/positioning/CenterWrapper';
+import ImageLayout from '~/components/layouts/ImageLayout';
+import TicketModal from '../../components/event/modal/TicketModal.vue';
+import ActionButton from '~/components/buttons/ActionButton'
+export default {
+    middleware: [
+        'registration/none',
+        'registration/checked-in',
 
-        ],
-        components: {
-            CenterWrapper,
-            ImageLayout
+    ],
+    components: {
+        CenterWrapper,
+        ImageLayout,
+        ActionButton
+    },
+    methods: {
+        openTicket() {
+            this.$modal.show(TicketModal, {}, {
+                draggable: true,
+                resizable: false,
+                width: '90%',
+                height: 'auto',
+                maxWidth: 350,
+                maxHeight: 500,
+                adaptive: true,
+            });
+        },
+        formatDate(d) {
+            return new Date(d).toLocaleDateString('sv-SE');
         }
+    },
+    computed: {
+        account_type_str() {
+            return this.$auth.state.user.student ? 'Elev' : 'Extern' ;
+        },
+        name() {
+            return this.$auth.state.user.name;
+        },
+        opensAt() {
+            return new Date(this.$store.state.event.details.start_date).toLocaleTimeString('sv-SE',{hour: '2-digit', minute:'2-digit'});
+        },
+        betweenDates() {
+            return this.formatDate(this.$store.state.event.details.start_date) +
+                " - " + this.formatDate(this.$store.state.event.details.end_date);
+        },
     }
+}
 </script>
 
 <style lang="scss" scoped>
