5d2609f3a8a8e7fac3ed99e2979641f9e96c930e
ludvig.bjorkman <luddenr1@live.se>
2020-01-21 13:05:55 +0100

    Auto updating time for notifications

--- components/notifications/notifications.vue
+++ components/notifications/notifications.vue
@@ -1,19 +1,31 @@
 <template>
   <div>
-    <notification v-for="(row, i) of data" :key="i" :data="row"/>
+    <notification v-for="(row, i) in data" :key="i" :data="row"/>
   </div>
 </template>
 
 <script>
 import Notification from '~/components/notifications/notification'
 export default {
+
   data() {
     return {
       data:
        [
         {
           title: 'Öppen Registrering',
-          body: 'Lagsregistreringen för CS:GO turneringen är öppen gå till entren för att anmäla erat lag'
+          body: 'Lagsregistreringen för CS:GO turneringen är öppen gå till entren för att anmäla erat lag',
+          created_at: 1579607002000
+        },
+        {
+          title: 'Öppen Registrering',
+          body: 'Lagsregistreringen för CS:GO turneringen är öppen gå till entren för att anmäla erat lag',
+          created_at: 1579520602000
+        },
+        {
+          title: 'Öppen Registrering',
+          body: 'Lagsregistreringen för CS:GO turneringen är öppen gå till entren för att anmäla erat lag',
+          created_at: 1579599802000
         },
       ],
 
