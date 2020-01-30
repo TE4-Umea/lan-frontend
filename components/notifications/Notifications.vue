5fbab31d45f459e21cb6cfe527cb0930f4c9340e
Elias Renman <elias.renman00@gmail.com>
2020-01-30 13:27:08 +0100

    updated notifications order

--- components/notifications/Notifications.vue
+++ components/notifications/Notifications.vue
@@ -1,29 +1,47 @@
 <!-- changed file names and git doesnt allow me to commit it -->
 <template>
-<div class="w-100">
+<div class="w-100 notifications">
     <div v-if="$store.state.event.notifications.length == 0">
         <h4>Laddar..</h4>
         <div class="spinner" />
     </div>
-    <notification v-for="(row, i) in $store.state.event.notifications" :key="i" :data="row"/>
+    <notification v-for="(row, i) in notifications" :key="i" :data="row"/>
 </div>
 </template>
 
 <script>
 import Notification from '~/components/notifications/Notification';
 export default {
-  mounted() {
+    data() {
+        return {
+            notifications: [],
+        }
+    },
+    created() {
+        this.$store.subscribe((mutation, state) => {
+            if (mutation.type === 'event/SET_NOTIFICATIONS' ||
+                mutation.type === 'event/ADD_NOTIFICATION'
+            ) {
+                this.notifications = state.event.notifications.slice().reverse();
+            }
+        })
+    },
+    mounted() {
         if(this.$store.state.event.notifications.length == 0) {
             this.$store.dispatch('event/GET_NOTIFICATIONS');
         }
-  },
-  components: {
-      Notification,
-  }
+    },
+    components: {
+        Notification,
+    }
 }
 
 </script>
 
-<style>
 
+<style lang="scss" scoped>
+.notifications {
+    max-height: 300px;
+    overflow-y: scroll;
+}
 </style>
