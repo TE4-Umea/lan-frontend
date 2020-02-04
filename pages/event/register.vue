a9cbaba85e5195692cd418011ec7388436c13d2f
Elias Renman <elias.renman00@gmail.com>
2020-02-04 15:16:01 +0100

    added last registration validation

--- pages/event/register.vue
+++ pages/event/register.vue
@@ -6,14 +6,20 @@
             <important title="Lanet öppnar: ">{{opensAt}}</important>
             <important title="När:">{{betweenDates}}</important>
             <important title="Anmälan stänger: ">{{closesAt}}</important>
-            <div>
+            <div v-if="!registrationClosed">
                 <h3>Innan du anmäler dig</h3>
                 <p class="d-inline">Se till att du läser </p>
                 <p class="d-inline clickable underline" @click="openRulesModal">lanets regler</p>
                 <p class="d-inline">innan du anmäler dig, reglerna går även att läsa efter du anmält dig.</p>
+                <action-button class="mt-3" title="TILL ANMÄLAN" @onAction="showRegister=true"></action-button>
             </div>
-            <action-button class="mt-3" title="TILL ANMÄLAN" @onAction="showRegister=true"></action-button>
+            <div class="pt-3" v-else>
+                <h1>Anmälan stängd</h1>
+                <p class="d-inline">Dessvärre så har anmälan stängt.<br> Vill du fortfarande komma på lanet så kan det tillkomma en éntre avgift.</p>
+                <p class="d-inline clickable underline" @click="openRulesModal"><br>Se även till att du läst reglerna.</p>
+            </div>
         </div>
+        
     </wrapper>
     <modals-container/>
     <register-modal :showRegister="showRegister" @openRules="openRulesModal" @close="showRegister=false"/>
@@ -30,9 +36,9 @@ import RulesModal from '~/components/event/modal/RulesModal.vue';
 import RegisterModal from '~/components/event/modal/RegisterModal';
 
 export default {
-    head () {
+    head() {
         return {
-        titleTemplate: 'Anmälan %s',
+            titleTemplate: 'Anmälan %s',
         }
     },
     middleware: [
@@ -66,19 +72,24 @@ export default {
             return n < 10 ? '0' + n : n;
         },
         formatDate(d) {
-            return new Date(d).toLocaleDateString('sv-SE');
+            return new Date(d + 'Z').toLocaleDateString('sv-SE');
         }
     },
     computed: {
+        registrationClosed() {
+            const closesAt = new Date(this.$store.state.event.details.registration_closes_at + 'Z');
+            console.log("registration closes at", closesAt);
+            return closesAt.getTime() <= (new Date()).getTime();
+        },
         opensAt() {
-            return new Date(this.$store.state.event.details.start_date).toLocaleTimeString('sv-SE',{hour: '2-digit', minute:'2-digit'});
+            return new Date(this.$store.state.event.details.start_date + 'Z').toLocaleTimeString('sv-SE',{hour: '2-digit', minute:'2-digit'});
         },
         betweenDates() {
             return this.formatDate(this.$store.state.event.details.start_date) +
                 " - " + this.formatDate(this.$store.state.event.details.end_date);
         },
         closesAt() {
-            return new Date(this.$store.state.event.details.registration_closes_at).toLocaleString('sv-SE', {dateStyle: 'short', timeStyle: 'short'});
+            return new Date(this.$store.state.event.details.registration_closes_at + 'Z').toLocaleString('sv-SE', {dateStyle: 'short', timeStyle: 'short'});
         }
     }
 }
