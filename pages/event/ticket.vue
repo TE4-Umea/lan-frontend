6a13cf0ddf2cc01055df051b7607f2b80825ad5e
Elias Renman <33152039+eliasrenman@users.noreply.github.com>
2020-02-05 10:38:58 +0100

    fixed stuff (#37)

--- pages/event/ticket.vue
+++ pages/event/ticket.vue
@@ -11,9 +11,9 @@
             Du är anmäld till lanet som</p>
             <p class="d-inline font-weight-bold" v-text="account_type_str" />
             <p class="d-inline"> mellan </p>
-            <p class="d-inline font-weight-bold">{{betweenDates}}</p>
-            <p class="d-inline">, lanet börjar </p>
-            <p class="d-inline font-weight-bold">{{ 'kl ' + opensAt}}</p>
+            <p class="d-inline font-weight-bold">{{betweenDates + ','}}</p>
+            <p class="d-inline"> lanet börjar </p>
+            <p class="d-inline font-weight-bold">{{ 'kl ' + opensAt + '.'}}</p>
 
 
             </image-layout>
