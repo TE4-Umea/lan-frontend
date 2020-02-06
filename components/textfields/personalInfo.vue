4f269788638d3598831c46f4e5a173018e5bbe06
Elias Renman <elias.renman00@gmail.com>
2020-02-06 08:24:53 +0100

    fixed personal info

--- components/textfields/personalInfo.vue
+++ components/textfields/personalInfo.vue
@@ -16,7 +16,7 @@
                 class="mb-0" 
                 v-text="'Din gruppkod är ' + $store.state.event.registration.group_code + '.'"
             />
-            <p class="mb-0" v-text="'Din plats är i ' + $store.state.event.registration.room.name + '.'"/>
+            <p v-if="$store.state.event.registration.room" class="mb-0" v-text="'Din plats är i ' + $store.state.event.registration.room.name + '.'"/>
             <p class="d-inline clickable underline" @click="openRulesModal">Här kommer du åt reglerna!</p>
         </div>
     </div>
